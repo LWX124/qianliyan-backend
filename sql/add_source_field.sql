4d18eb6adb53756202117febd922279ab262a192
lwx <liufeng124.lwx@gmail.com>
2026-04-05 20:04:22 +0800

    feat: 数据库迁移脚本 - app_user 和 app_b_user 增加 source 字段

--- sql/add_source_field.sql
+++ sql/add_source_field.sql
@@ -1,7 +1,14 @@
--- 千里眼小程序后端改动 SQL
--- 为事故记录表添加 source 字段，用于区分不同小程序来源
+-- 多小程序来源识别：增加 source 字段
+-- 执行时间：2026-04-05
 
-ALTER TABLE app_accident_record ADD COLUMN source VARCHAR(50) DEFAULT NULL COMMENT '来源标识，区分不同小程序';
+-- app_user 表增加 source 字段
+ALTER TABLE app_user ADD COLUMN source VARCHAR(20) DEFAULT NULL COMMENT '小程序来源标识';
+CREATE INDEX idx_app_user_source ON app_user(source);
 
--- 创建索引方便按来源查询
-ALTER TABLE app_accident_record ADD INDEX idx_source (source);
+-- app_b_user 表增加 source 字段
+ALTER TABLE app_b_user ADD COLUMN source VARCHAR(20) DEFAULT NULL COMMENT '小程序来源标识';
+CREATE INDEX idx_app_b_user_source ON app_b_user(source);
+
+-- 存量数据迁移：现有用户全部来自千里眼小程序
+UPDATE app_user SET source = 'SSP' WHERE source IS NULL;
+UPDATE app_b_user SET source = 'SSP' WHERE source IS NULL;
