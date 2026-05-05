242ae2a1fe9f44070a9a556ec49daab5ee02d631
lwx <liufeng124.lwx@gmail.com>
2026-05-05 11:48:38 +0800

    fix: 明确余额查询接口的 HTTP GET 方法

--- icars-master/icars-master/icars-admin/src/main/java/com/stylefeng/guns/modular/system/controller/AccidentController.java
+++ icars-master/icars-master/icars-admin/src/main/java/com/stylefeng/guns/modular/system/controller/AccidentController.java
@@ -29,6 +29,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -400,7 +401,7 @@ public class AccidentController extends BaseController {
     /**
      * 查询微信支付余额
      */
-    @RequestMapping("/balance")
+    @RequestMapping(value = "/balance", method = RequestMethod.GET)
     @Permission
     @ResponseBody
     public Object queryBalance() {
